df623ac6a62248cec416658a3a8a113ac2c32f74
reus <reusee@gmail.com>
2012-12-07 16:12:41 +0800

    extract method info

---
+++ codegen.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "./info"
+  "fmt"
+)
+
+func main() {
+  fmt.Printf("%d\n", len(info.methods))
+}
---
+++ info/type.go
@@ -0,0 +1,65 @@
+package info
+
+type Class struct {
+  name string
+  external bool
+  parents []*Class
+  size int
+  has_constructor bool
+  has_copy_constructor bool
+  has_virtual_destructor bool
+  is_namespace bool
+  is_defined_elsewhere bool
+}
+
+const (
+  T_VOIDP = 0
+  T_BOOL
+  T_CHAR
+  T_UCHAR
+  T_SHORT
+  T_USHORT
+  T_INT
+  T_UINT
+  T_LONG
+  T_ULONG
+  T_FLOAT
+  T_DOUBLE
+  T_ENUM
+  T_CLASS
+
+  KIND_STACK = iota
+  KIND_POINTER
+  KIND_REFERENCE
+  KIND_NONE
+)
+
+type Type struct {
+  name string
+  class *Class
+  typeId int
+  kind int
+  is_const bool
+}
+
+type Method struct {
+  class *Class
+  name string
+  arguments []*Type
+  ret *Type
+  is_static bool
+  is_const bool
+  is_copy_constructor bool
+  is_internal bool
+  is_enum bool
+  is_constructor bool
+  is_destructor bool
+  is_protected bool
+  is_field_accessor bool
+  is_property_accessor bool
+  is_virtual bool
+  is_purevirtual bool
+  is_signal bool
+  is_slot bool
+  is_explicit bool
+}
